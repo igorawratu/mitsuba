358e13d45dde42e66fd6e746f102ee69100e59a5
Sunrise Wang <suwang@inrialpes.fr>
2018-10-04 12:59:43 +0200

    wip lighttree

--- src/integrators/manylights/lighttree.cpp
+++ src/integrators/manylights/lighttree.cpp
@@ -1,5 +1,11 @@
 #include "lighttree.h"
 
+#include <tuple>
+#include <queue>
+#include <functional>
+
+MTS_NAMESPACE_BEGIN
+
 LightTree::LightTree() : point_tree_root_(nullptr), directional_tree_root_(nullptr), oriented_tree_root_(nullptr){
 
 }
@@ -44,8 +50,104 @@ void calculateSimilarityMatrices(const std::vector<VPL>& point_vpls, const std::
 	}
 }
 
-std::unique_ptr<LightTreeNode> createLightTree(const std::vector<VPL>& vpls, const std::vector<float>& similarity_matrix) {
+std::unique_ptr<LightTreeNode> createLightTree(const std::vector<VPL>& vpls, EVPLType vpl_type) {
+	if(vpls.size() == 0 || vpls[0].type != vpl_type){
+		return nullptr;
+	}
 
+	std::array<std::vector<std::unique_ptr<LightTreeNode>>, 2> nodes;
+	std::uint8_t current_level = 0;
+	
+	for(int i = 0; i < vpls.size(); ++i){
+		nodes[current_level].emplace_back(new LightTreeNode());
+
+		if(vpl_type != EDirectionalEmitterVPL){
+			nodes[current_level][i]->tl = vpls[i].its.p;
+			nodes[current_level][i]->br = vpls[i].its.p;
+		}
+		
+		if(vpl_type != EPointEmitterVPL){
+			nodes[current_level][i]->cone_ray1 = vpls[i].its.shFrame.n;
+			nodes[current_level][i]->cone_ray2 = vpls[i].its.shFrame.n;
+		}
+		
+	}
+
+	typedef std::tuple<float, std::uint32_t, std::uint32_t> SimEntry;
+
+	auto comparator = [](SimEntry l, SimEntry r){
+		return std::get<0>(l) < std::get<0>(r);
+	};
+
+	std::priority_queue<SimEntry, std::vector<SimEntry>, decltype(comparator)> similarity_matrix;
+	
+	while(true){
+		if(nodes[current_level].size() == 1){
+			break;
+		}
+
+		for(int i = 0; i < nodes[current_level].size(); ++i){
+			for(int j = i + 1; j < nodes[current_level].size(); ++j){
+				float d = 0.f;
+				if(vpl_type != EDirectionalEmitterVPL){
+					d += (nodes[current_level][i]->br - nodes[current_level][i]->tl).length();
+				}
+				
+				if(vpl_type != EPointEmitterVPL){
+					d += (1.f - dot(nodes[current_level][i]->cone_ray1, nodes[current_level][i]->cone_ray2)) / 0.5f;
+				}
+
+				similarity_matrix.push(std::make_tuple(d, i, j));
+			}
+		}
+
+		std::uint32_t nodes_left = nodes[current_level].size();
+		std::uint8_t next_level = (current_level + 1) % 2;
+
+		while(nodes_left > 0){
+			SimEntry entry = similarity_matrix.top();
+			similarity_matrix.pop();
+
+			std::uint32_t id1 = std::get<1>(entry), id2 = std::get<2>(entry);
+
+			if(nodes[current_level][id1] != nullptr && 
+				nodes[current_level][id2] != nullptr){
+				nodes[next_level].emplace_back(new LightTreeNode());
+				LightTreeNode *c1 = nodes[current_level][id1].get(), *c2 = nodes[current_level][id2].get();
+				
+				if(vpl_type != EDirectionalEmitterVPL){
+					nodes[next_level].back()->tl = Point(std::max(c1->tl[0], c2->tl[0]), 
+						std::max(c1->tl[1], c2->tl[1]), 
+						std::max(c1->tl[2], c2->tl[2]));
+					nodes[next_level].back()->br = Point(std::min(c1->br[0], c2->br[0]), 
+						std::min(c1->br[1], c2->br[1]), 
+						std::min(c1->br[2], c2->br[2]));
+				}
+				
+				//need to ideally find a better method to union 2 cones
+				if(vpl_type != EPointEmitterVPL){
+					nodes[next_level].back()->cone_ray1 = c1->cone_ray1;
+					nodes[next_level].back()->cone_ray2 = c1->cone_ray2;
+					
+					float dp1 = dot(nodes[next_level].back()->cone_ray1, nodes[next_level].back()->cone_ray2);
+					float dp2 = dot(nodes[next_level].back()->cone_ray1, c2->cone_ray1);
+					float dp3 = dot(nodes[next_level].back()->cone_ray1, c2->cone_ray2);
+
+					if(dp2 > dp1 || dp3 > dp1){
+						nodes[next_level].back()->cone_ray2 = d2 > dp3 ? c2->cone_ray1 : c2->cone_ray2;
+					}
+
+					dp1 = dot(nodes[next_level].back()->cone_ray1, nodes[next_level].back()->cone_ray2);
+					dp2 = dot(nodes[next_level].back()->cone_ray1, c2->cone_ray1);
+					dp3 = dot(nodes[next_level].back()->cone_ray1, c2->cone_ray2);
+					
+				}
+				
+			}
+		}
+	}
+
+	return std::move(nodes[current_level][0]);
 }
 
 LightTree::LightTree(const std::vector<VPL>& vpls) {
@@ -74,6 +176,28 @@ LightTree::LightTree(const LightTree& other) {
 
 LightTree::LightTree(const LightTree&& other) {
 
+				continue;
+		}
+	}
+
+	calculateSimilarityMatrices(point_vpls_, directional_vpls_, oriented_vpls_, point_similarity_matrix_, directional_similarity_matrix_, oriented_similarity_matrix_);
+}
+
+LightTree::LightTree(const LightTree& other) {
+
+}
+
+LightTree::LightTree(const LightTree&& other) {
+
+}
+
+LightTree& LightTree::operator = (const LightTree& other) {
+
+}
+
+LightTree& LightTree::operator = (LightTree&& other) {
+
+}
 }
 
 LightTree& LightTree::operator = (const LightTree& other) {
@@ -95,3 +219,5 @@ void LightTree::setVPLs(const std::vector<VPL>& vpls) {
 std::vector<VPL> LightTree::getClusteringForPoint(Intersection its) {
 
 }
+
+MTS_NAMESPACE_ENDvectorvector
--- src/integrators/manylights/lighttree.h
+++ src/integrators/manylights/lighttree.h
@@ -6,18 +6,22 @@
 #include <mitsuba/render/vpl.h>
 #include <mitsuba/render/shape.h>
 
+MTS_NAMESPACE_BEGIN
 
 struct LightTreeNode{
-    LightTreeNode() : left(nullptr), right(nullptr), vpl(nullptr), emission_scale(0.f){
+    LightTreeNode() : left(nullptr), right(nullptr), vpl(nullptr), emission_scale(0.f), tl(0.f), tr(0.f), cone_ray1(0.f),
+        cone_ray2(0.f){
     }
 
     LightTreeNode(const LightTreeNode& other) : left(other.left == nullptr ? nullptr : std::make_unique<LightTreeNode>(other.left)),
         right(other.right == nullptr ? nullptr : std::make_unique<LightTreeNode>(other.right)),
-        vpl(other.vpl), emission_scale(other.emission_scale), idx(other.idx){
+        vpl(other.vpl), emission_scale(other.emission_scale), tl(other.tl), br(other.br), cone_ray1(other.cone_ray1),
+        cone_ray2(other.cone_ray2){
     }
 
     LightTreeNode(LightTreeNode&& other) : left(std::move(other.left)), right(std::move(other.right)),
-        vpl(other.vpl), emission_scale(other.emission_scale), idx(other.idx){
+        vpl(other.vpl), emission_scale(other.emission_scale), tl(other.tl), br(other.br), cone_ray1(other.cone_ray1),
+        cone_ray2(other.cone_ray2){
     }
 
     LightTreeNode& operator = (const LightTreeNode& other){
@@ -26,7 +30,10 @@ struct LightTreeNode{
            right = other.right == nullptr ? nullptr : std::make_unique<LightTreeNode>(other.right);
            vpl = other.vpl;
            emission_scale = other.emission_scale;
-           idx = other.idx;
+           tl = other.tl;
+           br = other.br;
+           cone_ray1 = other.cone_ray1;
+           cone_ray2 = other.cone_ray2;
        }
 
        return *this;
@@ -34,11 +41,14 @@ struct LightTreeNode{
 
     LightTreeNode& operator = (LightTreeNode&& other){
         if(this != &other){
-           left = other.left == std::move(other.left);
-           right = other.right == std::move(other.right);
+           left = std::move(other.left);
+           right = std::move(other.right);
            vpl = other.vpl;
            emission_scale = other.emission_scale;
-           idx = other.idx;
+           tl = other.tl;
+           br = other.br;
+           cone_ray1 = other.cone_ray1;
+           cone_ray2 = other.cone_ray2;
        }
 
        return *this;
@@ -47,7 +57,8 @@ struct LightTreeNode{
     std::unique_ptr<LightTreeNode> left, right;
     VPL* vpl;
     float emission_scale;
-    std::uint32_t idx;
+    Point tl, br;
+    Vector3 cone_ray1, cone_ray2;
 };
 
 class LightTree{
@@ -65,8 +76,9 @@ public:
 
 private:
     std::vector<VPL> point_vpls_, directional_vpls_, oriented_vpls_;
-    std::vector<float> point_similarity_matrix_, directional_similarity_matrix_, oriented_similarity_matrix_;
     std::unique_ptr<LightTreeNode> point_tree_root_, directional_tree_root_, oriented_tree_root_;
-}
+};
+
+MTS_NAMESPACE_END
 
 #endif
