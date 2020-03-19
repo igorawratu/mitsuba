2e4e7f8f75b77635289b476d9bd8948b7e9de73f
Sunrise <sunrisewng@gmail.com>
2020-03-19 21:02:51 +0100

    more fixes

--- scenes/run_is_classroom.sh
+++ scenes/run_is_classroom.sh
@@ -9,7 +9,7 @@ rm "${source_dir}/classroom/samplerates"
 #IMPORTANCE SAMPLING
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.05 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.05 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_import_05"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_import_05"
@@ -17,7 +17,7 @@ mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_im
 
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.1 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.1 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_import_1"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_import_1"
@@ -25,25 +25,25 @@ mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_im
 
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.2 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.15 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=0 -Dvp=0
+    python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_import_15"
+done
+mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_import_15"
+mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_import_15"
+
+for i in {1..5}
+do
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.2 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_import_2"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_import_2"
 mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_import_2"
 
-for i in {1..5}
-do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.4 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0
-    python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_import_4"
-done
-mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_import_4"
-mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_import_4"
-
 
 #NO IMPORTANCE SAMPLING
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.05 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.05 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_nimport_05"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_nimport_05"
@@ -51,7 +51,7 @@ mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_ni
 
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.1 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.1 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_nimport_1"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_nimport_1"
@@ -59,16 +59,16 @@ mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_ni
 
 for i in {1..5}
 do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.2 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=1 -Dvp=0
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.15 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=0 -Dvp=0
+    python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_nimport_15"
+done
+mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_nimport_15"
+mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_nimport_15"
+
+for i in {1..5}
+do
+    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.2 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=0 -Dvp=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_nimport_2"
 done
 mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_nimport_2"
 mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_nimport_2"
-
-for i in {1..5}
-do
-    mitsuba "${source_dir}/classroom/scene.xml" -Dcstrat=3 -Dsamplerate=0.4 -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=0 -Dbv=1 -Dvp=0
-    python3 "${source_dir}/../compare_err.py" "${source_dir}/classroom/scene.exr" "${source_dir}/classroom/groundtruth.exr" "${source_dir}/classroom/classroom_nimport_4"
-done
-mv "${source_dir}/classroom/timings" "${source_dir}/classroom/timings_nimport_4"
-mv "${source_dir}/classroom/samplerates" "${source_dir}/classroom/samplerates_nimport_4"
