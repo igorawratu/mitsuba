f6ec3a6b6bf3459efec0e811340bd03d911c2d9c
Sunrise <sunrisewng@gmail.com>
2020-03-24 15:13:39 +0100

    wip

--- src/integrators/manylights/matrixreconstruction.cpp
+++ src/integrators/manylights/matrixreconstruction.cpp
@@ -105,9 +105,9 @@ Eigen::MatrixXf calculateClusterContributions(const std::vector<VPL>& vpls,
                 float r, g, b;
                 c.toLinearRGB(r, g, b);
 
-                contributions(i * 3, j) = r;
-                contributions(i * 3 + 1, j) = g;
-                contributions(i * 3 + 2, j) = b;
+                contributions(i * 3, j) += r;
+                contributions(i * 3 + 1, j) += g;
+                contributions(i * 3 + 2, j) += b;
             }
         }
     }
