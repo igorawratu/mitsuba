d5c890c304277921116b907191c967808d738099
Sunrise <sunrisewng@gmail.com>
2020-05-12 19:04:34 +0200

    wip

--- src/integrators/manylights/matrixreconstruction.cpp
+++ src/integrators/manylights/matrixreconstruction.cpp
@@ -1163,7 +1163,7 @@ std::vector<std::pair<std::int32_t, std::vector<std::uint32_t>>> computeMinHammi
         }
 
         if(matching || opposite){
-            idx = i + 1;
+            std::uint32_t idx = i + 1;
             if(opposite){
                 idx = -idx;
             }
@@ -1171,7 +1171,7 @@ std::vector<std::pair<std::int32_t, std::vector<std::uint32_t>>> computeMinHammi
             min_hamming_distances.push_back(std::make_pair(idx, herr));
         }
         else{
-            idx = i + 1;
+            std::uint32_t idx = i + 1;
             herr.push_back(0);
 
             min_hamming_distances.push_back(std::make_pair(idx, herr));
