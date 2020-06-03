3e2f6bf4be7969c5d772ec0af047efc277400aaf
Sunrise <sunrisewng@gmail.com>
2020-06-03 18:46:54 +0200

    wip

--- scenes/testloop.sh
+++ scenes/testloop.sh
@@ -4,7 +4,8 @@ source_dir=$(dirname "${source_path}")
 
 for i in {1..50}
 do
-    mitsuba "${source_dir}/kitchen/scene.xml" -Dcstrat=3 -Dsamplerate=$((0.01 * i)) -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0 -Diet=0 -Dge=0
+    sr="0.01 * i" | bc
+    mitsuba "${source_dir}/kitchen/scene.xml" -Dcstrat=3 -Dsamplerate="${sr}" -Dcompcstrat=mdlc -Dcps=1000 -Dsps=1 -Dspp=4 -Dslice=1000 -Dis=1 -Dbv=1 -Dvp=0 -Diet=0 -Dge=0
     python3 "${source_dir}/../compare_err.py" "${source_dir}/kitchen/scene.exr" "${source_dir}/kitchen/groundtruth.exr" "${source_dir}/kitchen/kitchen_boolge_1_1000"
 done
 mv "${source_dir}/kitchen/timings" "${source_dir}/kitchen/timings_boolge_1_1000"
